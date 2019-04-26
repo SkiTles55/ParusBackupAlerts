fde0d590119995c3d27a5a01f1214762c8032052
SkiTles <skitles55@gmail.com>
2019-04-26 14:24:50 +0600

    Добавьте файлы проекта.

---
+++ ParusBackupAlerts/MyCustomApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using ParusBackupAlerts.Properties;
+using System;
+using System.Windows.Forms;
+
+public class MyCustomApplicationContext : ApplicationContext
+{
+    private NotifyIcon trayIcon;
+
+    public MyCustomApplicationContext()
+    {
+        trayIcon = new NotifyIcon()
+        {
+            Icon = Resources.AppIcon,
+            Text = "ParusBackupHelper",
+            ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Выход", Exit)
+            }),
+            Visible = true
+        };
+    }
+
+    void Exit(object sender, EventArgs e)
+    {
+        trayIcon.Visible = false;
+        Application.Exit();
+    }
+
+    public void ShowMessage(string text, int time) => trayIcon.ShowBalloonTip(time, "Информация", text, ToolTipIcon.Warning);
+}
---
+++ ParusBackupAlerts/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParusBackupAlerts
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+
+        public static MyCustomApplicationContext icon;
+        public static Timer tmr;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            tmr = new Timer
+            {
+                Interval = 60000
+            };
+            tmr.Tick += new EventHandler(Tmr_Tick);
+            tmr.Start();
+            icon = new MyCustomApplicationContext();
+            Application.Run(icon);
+        }
+
+        static void Tmr_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now.Hour == Properties.Settings.Default.backuptime.Hour)
+            {
+                //check for apps and return if closed
+                Process[] pname = Process.GetProcessesByName("notepad"); // rename later
+                if (pname.Length == 0) return;
+                if (DateTime.Now.Minute == Properties.Settings.Default.backuptime.Minute)
+                {
+                    //close parus apps
+                }
+                else if (DateTime.Now.Minute <= Properties.Settings.Default.backuptime.Minute)
+                {
+                    //show alerts
+                    DateTime backupT = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, Properties.Settings.Default.backuptime.Hour, Properties.Settings.Default.backuptime.Minute, 0);
+                    switch ((backupT - DateTime.Now).Minutes)
+                    {
+                        case 10:
+                            ShowAlert(DateTime.Now.Minute);
+                            break;
+                        case 20:
+                            ShowAlert(DateTime.Now.Minute);
+                            break;
+                        case 30:
+                            ShowAlert(DateTime.Now.Minute);
+                            break;
+                    }
+                }
+            }
+        }
+
+        static void ShowAlert(int min)
+        {
+            Alert window = new Alert();
+            window.Show();
+        }
+    }
+}
